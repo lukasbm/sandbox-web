b0262aec04106ff5f502b2d5616b34d05102099a
Lukas <boehm-lukas@outlook.de>
2021-12-12 19:47:56 +0100

    :boom: introduce monaco editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { MonacoEditorModule } from "ngx-monaco-editor";
 
 import { AppComponent } from "./app.component";
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule],
+	declarations: [AppComponent, HeaderComponent],
+	imports: [BrowserModule, MonacoEditorModule.forRoot()],
 	providers: [],
 	bootstrap: [AppComponent],
 })
