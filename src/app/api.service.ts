c02122d2a3e9adda015d33278ade6fdf0c118803
Lukas <boehm-lukas@outlook.de>
2021-12-12 23:10:04 +0100

    :sparkles: add api service and work on header

---
+++ src/app/api.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+
+@Injectable({
+	providedIn: "root",
+})
+export class ApiService {
+	private base: string = environment.apiUrl.toString();
+
+	constructor(private http: HttpClient) {}
+
+	public getLanguages() {
+		return this.http.get(`${this.base}/getLanguages`);
+	}
+
+	public executeCode(language: string, code: string): Observable<any> {
+		return this.http.get(`${this.base}/executeCode`);
+	}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { MonacoEditorModule } from "ngx-monaco-editor";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { EditorComponent } from "./editor/editor.component";
-import { FormsModule } from "@angular/forms";
 
 @NgModule({
 	declarations: [AppComponent, HeaderComponent, EditorComponent],
-	imports: [BrowserModule, FormsModule, MonacoEditorModule.forRoot()],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		MonacoEditorModule.forRoot(),
+		HttpClientModule,
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
